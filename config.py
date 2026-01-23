354ab4ae73fbe6c2d2ba717e89af9aef69137af0
kanzedin <kanzedinakmel0@gmail.com>
2026-01-23 17:19:47 +0300

    hi

--- config.py
+++ config.py
@@ -6,11 +6,6 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
 
-# Chapa Payment Gateway
-CHAPA_SECRET_KEY = os.getenv('Test_Secret_key')
-CHAPA_PUBLIC_KEY = os.getenv('Test_Public_key')
-CHAPA_BASE_URL = "https://api.chapa.co/v1"
-
 # Werabe location boundaries
 MIN_LAT, MAX_LAT = 7.8500, 8.0000
 MIN_LON, MAX_LON = 38.0000, 38.2000
